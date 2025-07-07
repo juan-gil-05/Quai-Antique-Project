74ff96c7854742991f25008c9ae3167edcc30f71
juan-gil-05 <gilj06@hotmail.com>
2025-07-07 14:11:59 +0200

    Add initial project structure with routing and basic pages

---
+++ Router/allRoutes.js
@@ -0,0 +1,8 @@
+import Route from "./Route.js"
+
+export const allRoutes = [
+    new Route("/", "Accueil", "/Pages/home.html"),
+    new Route("/accueil", "Accueil", "/Pages/home.html")
+]
+
+export const websiteName = "Quai Antique"
