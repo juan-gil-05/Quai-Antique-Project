8962c4dce9c3b1ccc74fe56c7ecc1327407b09f8
juan-gil-05 <gilj06@hotmail.com>
2025-07-08 00:33:33 +0200

    Implement login functionality with credential validation and add logout feature

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/Pages/home.html"),
     new Route("/accueil", "Accueil", "/Pages/home.html"),
     new Route("/galerie", "Galerie", "/Pages/galery.html"),
-    new Route("/login", "Connexion", "/Pages/auth/login.html"),
+    new Route("/login", "Connexion", "/Pages/auth/login.html", "Js/auth/login.js"),
     new Route("/signup", "Inscription", "/Pages/auth/signup.html", "Js/auth/signup.js"),
     new Route("/account", "Mon compte", "/Pages/account.html"),
     new Route("/editPassword", "Modifier le mot de passe", "/Pages/auth/editPassword.html"),
