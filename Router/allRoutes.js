180f6243dfaa448d37faf98b0d1e39c4fbfdd0dc
juan-gil-05 <gilj06@hotmail.com>
2025-07-07 16:30:15 +0200

    Add account and edit password pages with routing updates

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
     new Route("/galerie", "Galerie", "/Pages/galery.html"),
     new Route("/login", "Connexion", "/Pages/login.html"),
     new Route("/signup", "Inscription", "/Pages/signup.html"),
+    new Route("/account", "Mon compte", "/Pages/account.html"),
+    new Route("/editPassword", "Modifier le mot de passe", "/Pages/editPassword.html"),
 ]
 
 export const websiteName = "Quai Antique"
