a4881d426d4dbd2cd8f1e264683c2be5fd5bb9d0
juan-gil-05 <gilj06@hotmail.com>
2025-07-07 16:17:15 +0200

    Add login page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/Pages/home.html"),
     new Route("/accueil", "Accueil", "/Pages/home.html"),
     new Route("/galerie", "Galerie", "/Pages/galery.html"),
+    new Route("/login", "Connexion", "/Pages/login.html"),
 ]
 
 export const websiteName = "Quai Antique"
