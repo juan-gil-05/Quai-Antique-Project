652f37bee39f74a232065a58b83dd47e0f20d50f
juan-gil-05 <gilj06@hotmail.com>
2025-07-07 16:10:26 +0200

    Add gallery page with style

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,8 @@ import Route from "./Route.js"
 
 export const allRoutes = [
     new Route("/", "Accueil", "/Pages/home.html"),
-    new Route("/accueil", "Accueil", "/Pages/home.html")
+    new Route("/accueil", "Accueil", "/Pages/home.html"),
+    new Route("/galerie", "Galerie", "/Pages/galery.html"),
 ]
 
 export const websiteName = "Quai Antique"
