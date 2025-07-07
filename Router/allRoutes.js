ca0a84f1a1a1f4d907eb7f5b3ccaffe0b5293882
juan-gil-05 <gilj06@hotmail.com>
2025-07-07 16:21:06 +0200

    Add signup page and update routing

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/accueil", "Accueil", "/Pages/home.html"),
     new Route("/galerie", "Galerie", "/Pages/galery.html"),
     new Route("/login", "Connexion", "/Pages/login.html"),
+    new Route("/signup", "Inscription", "/Pages/signup.html"),
 ]
 
 export const websiteName = "Quai Antique"
