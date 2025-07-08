e5d94b04d0bfe7796a470bdcec5c29632bde3287
juan-gil-05 <gilj06@hotmail.com>
2025-07-09 00:27:22 +0200

    Add of fetch API to backend for the signup function and add guest number input

--- Js/auth/signup.js
+++ Js/auth/signup.js
@@ -5,23 +5,29 @@ const inputPreNom = document.getElementById("PrenomInput")
 const inputMail = document.getElementById("EmailInput")
 const inputPassword = document.getElementById("PasswordInput")
 const inputValidationPassword = document.getElementById("ValidatePasswordInput")
+const inputNbConvives = document.getElementById("NbConvivesInput")
 const btnValidation = document.getElementById('btn-validate-signup')
+const formInscription = document.getElementById("form-inscription")
 
 inputNom.addEventListener("keyup", validateForm)
 inputPreNom.addEventListener("keyup", validateForm)
 inputMail.addEventListener("keyup", validateForm)
 inputPassword.addEventListener("keyup", validateForm)
 inputValidationPassword.addEventListener("keyup", validateForm)
+inputNbConvives.addEventListener("keyup", validateForm)
+
+btnValidation.addEventListener("click", userInscription)
 
 //Function permettant de valider tout le formulaire
 function validateForm() {
     const nomOk = validateRequired(inputNom)
     const prenomOk = validateRequired(inputPreNom)
+    const nbConvivesOk = validateRequired(inputNbConvives)
     const mailOk = validateMail(inputMail)
     const passwordOk = validatePassword(inputPassword)
     const confirmPasswordOk = validateConfirmationPassword(inputPassword, inputValidationPassword)
 
-    if(nomOk && prenomOk && mailOk && passwordOk && confirmPasswordOk){
+    if (nomOk && prenomOk && mailOk && nbConvivesOk && passwordOk && confirmPasswordOk) {
         btnValidation.disabled = false
     } else (
         btnValidation.disabled = true
@@ -42,47 +48,75 @@ function validateRequired(input) {
     }
 }
 
-function validateMail(input){
+function validateMail(input) {
     //Définir le regex
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     const mailUser = input.value
-    if(mailUser.match(emailRegex)){
+    if (mailUser.match(emailRegex)) {
         input.classList.add("is-valid")
-        input.classList.remove("is-invalid") 
+        input.classList.remove("is-invalid")
         return true
     }
-    else{
+    else {
         input.classList.remove("is-valid")
         input.classList.add("is-invalid")
         return false
     }
 }
 
-function validatePassword(input){
+function validatePassword(input) {
     //Définir le regex
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/
     const passwordUser = input.value
-    if(passwordUser.match(passwordRegex)){
+    if (passwordUser.match(passwordRegex)) {
         input.classList.add("is-valid")
-        input.classList.remove("is-invalid") 
+        input.classList.remove("is-invalid")
         return true
     }
-    else{
+    else {
         input.classList.remove("is-valid")
         input.classList.add("is-invalid")
         return false
     }
 }
 
-function validateConfirmationPassword(inputPwd, inputConfirmPwd){
-    if(inputPwd.value == inputConfirmPwd.value){
+function validateConfirmationPassword(inputPwd, inputConfirmPwd) {
+    if (inputPwd.value == inputConfirmPwd.value) {
         inputConfirmPwd.classList.add("is-valid");
         inputConfirmPwd.classList.remove("is-invalid");
         return true;
     }
-    else{
+    else {
         inputConfirmPwd.classList.add("is-invalid");
         inputConfirmPwd.classList.remove("is-valid");
         return false;
     }
+}
+
+function userInscription() {
+
+    const dataForm = new FormData(formInscription)
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "email": dataForm.get("email"),
+        "password": dataForm.get("password"),
+        "first_name": dataForm.get("first-name"),
+        "last_name": dataForm.get("last-name"),
+        "guest_number": dataForm.get("guest_number")
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("http://127.0.0.1:8000/api/registration", requestOptions)
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
 }
