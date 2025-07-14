cca2ac2118b995e3ce1279e4743f87505f700d9f
juan-gil-05 <gilj06@hotmail.com>
2025-07-14 15:33:22 +0200

    Add of validation for user edit form

--- Js/account.js
+++ Js/account.js
@@ -4,6 +4,36 @@ const inputAllergy = document.getElementById("input-allergy")
 const inputGuestNumber = document.getElementById("input-guestNumber")
 const editAccountBtn = document.getElementById("editAccountBtn")
 
+inputLastname.addEventListener("keyup", validateForm)
+inputName.addEventListener("keyup", validateForm)
+inputGuestNumber.addEventListener("keyup", validateForm)
+
+//Function permettant de valider tout le formulaire
+function validateForm() {
+    const nomOk = validateRequired(inputLastname)
+    const prenomOk = validateRequired(inputName)
+    const guestNumberOk = validateRequired(inputGuestNumber)
+
+    if (nomOk && prenomOk && guestNumberOk) {
+        editAccountBtn.disabled = false
+    } else {
+        editAccountBtn.disabled = true
+    }
+
+}
+
+function validateRequired(input) {
+    if (input.value != "") {
+        input.classList.add("is-valid")
+        input.classList.remove("is-invalid")
+        return true
+    } else {
+        input.classList.remove("is-valid")
+        input.classList.add("is-invalid")
+        return false
+    }
+}
+
 async function getUserInfo() {
     const userInfo = await getInfosUser()
 
