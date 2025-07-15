751f0422eb2729b157fc5635613e893d425ff5b7
juan-gil-05 <gilj06@hotmail.com>
2025-07-15 13:01:42 +0200

    Add of edit and delete action buttons in menu page

--- Js/menu/menu.js
+++ Js/menu/menu.js
@@ -1,8 +1,20 @@
 
 const allFood = document.getElementById("allFood")
 const allMenus = document.getElementById("allMenus")
+const editFoodTitleInput = document.getElementById("editFoodTitleInput")
+const editFoodDescriptionInput = document.getElementById("editFoodDescriptionInput")
+const editFoodPriceInput = document.getElementById("editFoodPriceInput")
+const deleteFoodTitleInput = document.getElementById("deleteFoodTitleInput")
+const deleteFoodDescriptionInput = document.getElementById("deleteFoodDescriptionInput")
+const deleteFoodPriceInput = document.getElementById("deleteFoodPriceInput")
+const editMenuTitleInput = document.getElementById("editMenuTitleInput")
+const editMenuDescriptionInput = document.getElementById("editMenuDescriptionInput")
+const editMenuPriceInput = document.getElementById("editMenuPriceInput")
+const deleteMenuTitleInput = document.getElementById("deleteMenuTitleInput")
+const deleteMenuDescriptionInput = document.getElementById("deleteMenuDescriptionInput")
+const deleteMenuPriceInput = document.getElementById("deleteMenuPriceInput")
 
-function getAllFood() {
+async function getAllFood() {
 
     const myHeaders = new Headers
     myHeaders.append("X-AUTH-TOKEN", getToken())
@@ -13,16 +25,28 @@ function getAllFood() {
         redirect: "follow"
     }
 
-    fetch(apiUrl + "food/all", requestOptions)
+    await fetch(apiUrl + "food/all", requestOptions)
         .then(result => result.json())
         .then(data => {
             const html = data.map(food => {
-                return `<div class="list-group-item list-group-item-action p-3">
+                return `<div class="food-list list-group-item list-group-item-action p-3">
                             <div class="d-flex w-100 justify-content-between">
                                 <h5 class="mb-1 fw-semibold">${food.title}</h5>
                                 <small>${food.price} €</small>    
                             </div>
                             <p class="mb-1">${food.description}</p>
+                            <div class="action-buttons" data-show="admin">
+                                <button type="button" class="btn btn-outline-dark btn-edit" id="editFoodAction"
+                                    data-title="${food.title}" data-description="${food.description}" data-price="${food.price}" data-bs-toggle="modal"
+                                    data-bs-target="#EditionFoodModal">
+                                    <i class="bi bi-pencil-square"></i>
+                                </button>
+                                <button type="button" class="btn btn-outline-dark btn-delete" id="deleteFoodAction"
+                                    data-title="${food.title}" data-description="${food.description}" data-price="${food.price}" data-bs-toggle="modal"
+                                    data-bs-target="#DeleteFoodModal">
+                                    <i class="bi bi-trash"></i>
+                                </button>
+                            </div>
                         </div>`
             }).join("")
 
@@ -30,7 +54,7 @@ function getAllFood() {
         })
 }
 
-function getAllMenus() {
+async function getAllMenus() {
     const myHeaders = new Headers
     myHeaders.append("X-AUTH-TOKEN", getToken())
 
@@ -40,7 +64,7 @@ function getAllMenus() {
         redirect: "follow"
     }
 
-    fetch(apiUrl + "menu/all", requestOptions)
+    await fetch(apiUrl + "menu/all", requestOptions)
         .then(result => result.json())
         .then(data => {
             const html = data.map(menu => {
@@ -48,6 +72,18 @@ function getAllMenus() {
                             <div class="card-body">
                                 <h5 class="card-title text-success">${menu.title} - ${menu.price} €</h5>
                                 <p class="card-text">${menu.description}</p>
+                                <div class="action-buttons" data-show="admin">
+                                <button type="button" class="btn btn-outline-dark btn-edit" id="editMenuAction"
+                                    data-title="${menu.title}" data-description="${menu.description}" data-price="${menu.price}" data-bs-toggle="modal"
+                                    data-bs-target="#EditionMenuModal">
+                                    <i class="bi bi-pencil-square"></i>
+                                </button>
+                                <button type="button" class="btn btn-outline-dark btn-delete" id="deleteMenuAction"
+                                    data-title="${menu.title}" data-description="${menu.description}" data-price="${menu.price}" data-bs-toggle="modal"
+                                    data-bs-target="#DeleteMenuModal">
+                                    <i class="bi bi-trash"></i>
+                                </button>
+                            </div>
                             </div>
                         </div>`
             }).join("")
@@ -56,6 +92,67 @@ function getAllMenus() {
         })
 }
 
-getAllFood()
-getAllMenus()
+// Fonction pour envoyer à chaque plat, les données correspondants
+function ButtonFoodEvents() {
+    document.querySelectorAll(".btn-edit").forEach(btn => {
+        btn.addEventListener("click", () => {
+            const title = btn.dataset.title
+            const description = btn.dataset.description
+            const price = btn.dataset.price
 
+            editFoodTitleInput.value = title
+            editFoodDescriptionInput.value = description
+            editFoodPriceInput.value = price
+        })
+    })
+
+    document.querySelectorAll(".btn-delete").forEach(btn => {
+        btn.addEventListener("click", () => {
+            const title = btn.dataset.title
+            const description = btn.dataset.description
+            const price = btn.dataset.price
+
+            deleteFoodTitleInput.value = title
+            deleteFoodDescriptionInput.value = description
+            deleteFoodPriceInput.value = price
+        })
+    })
+}
+// Fonction pour envoyer à chaque menu, les données correspondants
+function ButtonMenuEvents() {
+    document.querySelectorAll(".btn-edit").forEach(btn => {
+        btn.addEventListener("click", () => {
+            const title = btn.dataset.title
+            const description = btn.dataset.description
+            const price = btn.dataset.price
+
+            editMenuTitleInput.value = title
+            editMenuDescriptionInput.value = description
+            editMenuPriceInput.value = price
+        })
+    })
+
+    document.querySelectorAll(".btn-delete").forEach(btn => {
+        btn.addEventListener("click", () => {
+            const title = btn.dataset.title
+            const description = btn.dataset.description
+            const price = btn.dataset.price
+
+            deleteMenuTitleInput.value = title
+            deleteMenuDescriptionInput.value = description
+            deleteMenuPriceInput.value = price
+        })
+    })
+}
+
+// pour charger les deux fonctions
+async function loadFullMenu() {
+
+    await getAllFood()
+    await getAllMenus()
+
+    ButtonFoodEvents()
+    ButtonMenuEvents()
+}
+
+loadFullMenu()
